36e727c6d18d4c99228e2688de8f49e5c8a7a547
Heitor Brandão <heitor.souza21@gmail.com>
2020-08-26 10:15:25 -0400

    Iniciando template do painel administrativo

--- code/template/admin.tpl.php
+++ code/template/admin.tpl.php
@@ -1,12 +1,53 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Painel administrativo</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Painel Administrativo da School of Net</title>
 </head>
+
 <body>
-    <h1>Administração</h1>
-    <?php include $content; ?>
+    <div id="header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="" class="navbar-brand">AdminSON</a>
+            <span class="navbar-text">
+                Painel Administrativo da School of Net
+            </span>
+        </nav>
+    </div>
+    <div id="main">
+        <div class="row">
+            <div class="col">
+                <ul class="nav flex-column nav-pills bg-secondary text white p-2">
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50"><small>MENU</small></span>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="">Páginas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Usuários</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-10">
+                <?php include $content; ?>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
 </html>
