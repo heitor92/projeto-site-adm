c2fc9038e014bbbf9e3802b760fae4674b8d96a5
Heitor de Souza <heitor.souza21@gmail.com>
2020-09-01 17:44:52 -0400

    Template de formulário de cadastro de página

--- code/template/admin/pages/create.tpl.php
+++ code/template/admin/pages/create.tpl.php
@@ -1,6 +1,32 @@
 <h3>Administração de páginas</h3>
 
 
-// criação de página
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" class="form-control"
+            placeholder="Aqui vai o título da página...">
+    </div>
 
-<a href="/admin">Ir para home</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input name="url" id="pagesUrl" type="text" class="form-control"
+                placeholder="URL amigável, deixe em branco para informar a página inicial...">
+        </div>
+    </div>
+
+    <div class="form-group">
+        // editor de texto
+    </div>
+
+    <button type="submit" class="btn btn-primary">Salvar</button>
+
+</form>
+
+<hr>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
--- code/template/admin/pages/index.tpl.php
+++ code/template/admin/pages/index.tpl.php
@@ -1,6 +1,22 @@
-<h3>Administração de páginas</h3>
+<h3 class="mb-5">Administração de páginas</h3>
 
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>título</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td><a href="/admin/pages/1">Página inicial</a></td>
+            <td class="text-right">
+                <a href="/admin/pages/1" class="btn btn-primary btn-sm">ver</a>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
-// listagem
-
-<a href="/admin">Ir para home</a>
+<a href="/admin/pages/create" class="btn btn-secondary">Novo</a>
